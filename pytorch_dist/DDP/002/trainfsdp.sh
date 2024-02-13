17c9f3fe0db481ed3a6230cd41a2073f2216d5fb
Diego Andrade Canosa <diego.andrade@udc.es>
2024-02-13 07:54:07 +0100

    DDP

---
+++ pytorch_dist/DDP/002/trainfsdp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#module reset
+#module load miniconda3
+#conda activate mytorch
+#srun python mnist_classify_ddp.py --epochs=2
+#source $STORE/mytorchdist/bin/activate
+#conda activate mytorchdist
+#source $STORE/conda/envs/mytorchdist/bin/activate
+which python
+python fsdp.py --epochs=2
+#torchrun mnist_classify_ddp.py --epochs=2
+
