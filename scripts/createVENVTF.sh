e73ece2253e342194190422857633ff1dbb42449
Diego Andrade Canosa <diego.andrade@udc.es>
2024-02-23 17:32:02 +0100

    TF script creation

---
+++ scripts/createVENVTF.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+module load python/3.9
+rm -rf $STORE/mytf
+python -m venv $STORE/mytf
+source $STORE/mytf/bin/activate
+python3 -m pip install tensorflow[and-cuda]
+pip install jupyterlab-nvdashboard
+pip install -U tensorboard-plugin-profile
+pip install nvidia-pyindex
+pip install nvidia-cuda-cupti
+pip install matplotlib
+pip install portpicker
+pip install tensorflow-datasets
